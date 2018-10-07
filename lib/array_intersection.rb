6789ef96d6c27716c8ed5662c0c4f2b67ce7c9ce
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-07 10:53:51 -0700

    array_intersection jessie

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,22 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  return [] if array1 == nil || array2 == nil
+
+  result = []
+
+  i = 0
+
+  while i < array1.length
+    j = 0
+    while j < array2.length
+      if array1[i] == array2[j]
+        result << array1[i]
+        break
+      else
+        j += 1
+      end
+    end
+    i += 1
+  end
+  return result
 end
